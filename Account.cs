63ddcecc81d347182c03fccc517e22fb182c4a43
Lenovo <Lenovo@DESKTOP-067GH63>
2020-12-10 13:28:12 +0330

    simple observer pattern

---
+++ Account.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Observer_Pattern
+{
+    public class Account
+    {
+        private long _balance;
+        public long Balance { get { return _balance; } private set { } }
+
+        public event EventHandler<BalanceChangedEventArgs> BalanceChanged;
+
+        public void Deposit(long amount)
+        {
+            if (amount > 0)
+                _balance += amount;
+
+            OnBalanceChanged(new BalanceChangedEventArgs(_balance));
+        }
+
+        public void Withdrew(long amount)
+        {
+            if (amount <= _balance)
+                _balance -= amount;
+
+            OnBalanceChanged(new BalanceChangedEventArgs(_balance));
+        }
+
+        private void OnBalanceChanged(BalanceChangedEventArgs e)
+        {
+            BalanceChanged?.Invoke(this, e);
+        }
+    }
+
+    public class BalanceChangedEventArgs : EventArgs
+    {
+        public DateTime When { get; private set; }
+        public long NewBalance { get; private set; }
+
+        public BalanceChangedEventArgs(long newBalance)
+        {
+            When = DateTime.Now;
+            NewBalance = newBalance;
+        }
+    }
+    
+    //Observers/Subscribers
+    public class Emailer
+    {
+        public Emailer(Account account)
+        {
+            account.BalanceChanged += Account_BalanceChanged;
+        }
+
+        private void Account_BalanceChanged(object sender, BalanceChangedEventArgs e)
+        {
+            Console.WriteLine($"Sending Email => Changed Balance {e.NewBalance} at {e.When}");
+        }
+    }
+
+    //Observers/Subscribers
+    public class Logger
+    {
+        public Logger(Account account)
+        {
+            account.BalanceChanged += Account_BalanceChanged;
+        }
+
+        private void Account_BalanceChanged(object sender, BalanceChangedEventArgs e)
+        {
+            Console.WriteLine($"Logging => Changed Balance {e.NewBalance} at {e.When}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace Observer_Pattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var account = new Account();
+            var emailer = new Emailer(account);
+            var logger = new Logger(account);
+
+            account.Deposit(20);
+            account.Deposit(10);
+            account.Deposit(50);
+
+            account.Withdrew(70);
+            account.Deposit(23);
+            account.Withdrew(17);
+
+            Console.ReadLine();
         }
     }
 }
